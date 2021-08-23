76c844eedd81ee5a16c513472725d17816561dd8
shahbazhassan42000 <shahbazhassan42000@gmail.com>
2021-08-24 01:00:18 +0500

    added js file for the toggle of google-apps

---
+++ js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded",function () {
+   //google-apps toggle
+   let google_apps_btn=document.querySelector(".google-apps-img");
+   let google_apps_img_container=document.querySelector(".google-apps-container");
+   google_apps_btn.addEventListener('click',function () {
+      if(google_apps_img_container.style.display==="block")
+      {
+         console.log("Hiding!!!");
+         google_apps_img_container.style.display="none";
+      }
+      else
+      {
+         console.log("Showing!!!");
+         google_apps_img_container.style.display="block";
+      }
+   });
+   document.addEventListener('mouseup',function (event) {
+      if(!google_apps_img_container.contains(event.target) && event.target!=google_apps_btn)
+      {
+         console.log("Hiding!!! by blur")
+         google_apps_img_container.style.display="none";
+      }
+   });
+
+
+});
