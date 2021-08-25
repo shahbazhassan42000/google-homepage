42210623631b62a68810955a021c8700351bd9d5
Shahbaz Hassan <shahbazhassan42000@gmail.com>
2021-08-26 00:53:18 +0500

    added js for Google account toggle

--- js/script.js
+++ js/script.js
@@ -4,21 +4,26 @@ document.addEventListener("DOMContentLoaded",function () {
    let google_apps_img_container=document.querySelector(".google-apps-container");
    google_apps_btn.addEventListener('click',function () {
       if(google_apps_img_container.style.display==="block")
-      {
-         console.log("Hiding!!!");
          google_apps_img_container.style.display="none";
-      }
       else
-      {
-         console.log("Showing!!!");
          google_apps_img_container.style.display="block";
-      }
    });
    document.addEventListener('mouseup',function (event) {
       if(!google_apps_img_container.contains(event.target) && event.target!=google_apps_btn)
-      {
-         console.log("Hiding!!! by blur")
          google_apps_img_container.style.display="none";
-      }
    });
+   //google-account toggle
+   let google_account_btn=document.querySelector("#google-account-button");
+   let google_account_container=document.querySelector(".google-account-container");
+   google_account_btn.addEventListener('click',function () {
+      if(google_account_container.style.display==="block")
+         google_account_container.style.display="none";
+      else
+         google_account_container.style.display="block";
+   });
+   document.addEventListener('mouseup',function (event) {
+      if(!google_account_container.contains(event.target) && event.target!=google_account_btn)
+         google_account_container.style.display="none";
+   });
+
 });
