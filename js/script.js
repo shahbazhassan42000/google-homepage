0470c323e43b7c82992c1b799023fbb22dd17890
Shahbaz Hassan <shahbazhassan42000@gmail.com>
2021-08-27 01:56:16 +0500

    just fix on toggle google app and google account to fix the buttons actually links as I used links here but focus work on input elements so make a css class "focus" and added in the link element on toggle.

--- js/script.js
+++ js/script.js
@@ -4,26 +4,44 @@ document.addEventListener("DOMContentLoaded",function () {
    let google_apps_img_container=document.querySelector(".google-apps-container");
    google_apps_btn.addEventListener('click',function () {
       if(google_apps_img_container.style.display==="block")
+      {
          google_apps_img_container.style.display="none";
+         google_apps_btn.classList.remove("focus");
+      }
       else
+      {
          google_apps_img_container.style.display="block";
+         google_apps_btn.classList.add("focus");
+      }
    });
    document.addEventListener('mouseup',function (event) {
       if(!google_apps_img_container.contains(event.target) && event.target!=google_apps_btn)
+      {
          google_apps_img_container.style.display="none";
+         google_apps_btn.classList.remove("focus");
+      }
    });
    //google-account toggle
    let google_account_btn=document.querySelector("#google-account-button");
    let google_account_container=document.querySelector(".google-account-container");
    google_account_btn.addEventListener('click',function () {
       if(google_account_container.style.display==="block")
+      {
          google_account_container.style.display="none";
+         google_account_btn.classList.remove("focus");
+      }
       else
+      {
          google_account_container.style.display="block";
+         google_account_btn.classList.add("focus");
+      }
    });
    document.addEventListener('mouseup',function (event) {
       if(!google_account_container.contains(event.target) && event.target!=google_account_btn)
+      {
          google_account_container.style.display="none";
+         google_account_btn.classList.remove("focus");
+      }
    });
 
 });
